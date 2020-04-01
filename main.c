91c233572de782439c80051b1171fe4c95cdf348
OmarMGhanem <ghanem374@gmail.com>
2020-04-01 14:48:03 +0200

    Finish parsing

--- main.c
+++ main.c
@@ -4,20 +4,28 @@
 #include <unistd.h>
 #include <string.h>
 void type_prompt(void) ;
-void read_command(char cmd []  , char** parameters) ;
+void read_command(char *input, char **parameters) ;
 
 int main()
 {
     pid_t fork_return;
-    char command [100] , char* parameters [20] ;
+    char *input ;
+    char  *parameters[64];
 
-while(1)
-{
-    //type prompt
-    type_prompt() ;
-    //read input
-    read_command( command ,parameters ) ;
+
+input = malloc(4098);
+
+
+
+
+    while(1){
+            type_prompt() ;
+            fgets(input, 4098,stdin);
+            read_command(input ,parameters);
+
+
     //forking
+
     fork_return = fork() ;
 
     if (fork_return == -1)
@@ -27,19 +35,71 @@ while(1)
     }else if (fork_return == 0 )
     {
         //child process excution
-        //excepv(command, parameters) ;
+
+        execvp(parameters[0], parameters) ;
+        printf("\n");
+
     }else
     {
         //parent process excution
-        if ( strcmp(command , ) == "exit"  )
-        {
-            break ;
+
+
+
+
+
+        if ( strcmp(parameters[0] ,"exit")==0 )
+        {   free(input);
+            exit(0);
+
         }
 
-    }
+    }}
 
 
-}
 
+free(input);
     return 0;
 }
+
+
+
+
+void type_prompt(void){
+printf("ALOShell $ ");
+
+}
+
+void read_command(char*input , char **parameters){
+
+
+ while (*input != '\0') {       /* if not the end of input ....... */
+          while (*input == ' ' || *input == '\t' || *input == '\n')
+               *input++ = '\0';     /* replace white spaces with 0    */
+          *parameters++ = input;          /* save the argument position     */
+          while (*input != '\0' && *input != ' ' &&
+                 *input != '\t' && *input != '\n')
+               input++;             /* skip the argument until ...    */
+     }
+     *(parameters-1) = NULL;                 /* mark the end of argument list  */
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
