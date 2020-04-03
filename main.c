08886bb1433bfdbb727629571bfc0a4cdde5452c
OmarMGhanem <ghanem374@gmail.com>
2020-04-03 22:06:38 +0200

    I've made the code cleaner,also i've add some function  add an input check to make sure we don't get segmentation error for white spaces

--- main.c
+++ main.c
@@ -16,7 +16,11 @@
 
 void type_prompt(void) ; // Fuction which called to promet the fixed header "ALO SHELL $" also used for color formating
 void parsing_command(char *input, char **parameters) ; // This Function handle the input and point to every argument start
+unsigned char read_error_check(char *input); // this fuucntion check the input for error to avoid segmentation errors !
+void read_command(char *input); // this fuction read the input
 unsigned char  wait_flag  = 0 ; // simple flag to chech whether the parent MUST wait or not
+const char * color_string = "--color"; // the color argument for LS which is add in the parsing part if the command is LS
+
 int main()
 {
     pid_t fork_return; // shall hold process id
@@ -30,56 +34,48 @@ input = malloc(4098);   // Dynamic allocation for the input to avoid  memory Run
 
 // The shell core
     while(1){
-            type_prompt() ;
-            fgets(input, 4098,stdin); //Reads the input MAX size is 4098
-            parsing_command(input ,parameters);
 
-
-
+        read_command(input);
+        parsing_command(input ,parameters);
 
     //forking
 
-    fork_return = fork() ; // save the fork order for furtur checks
+        fork_return = fork() ; // save the fork order for furtur checks
 
-    if (fork_return == -1)
-    {
-printf("Forking failed,Child process can't be created \n");
+        if (fork_return == -1)
+        {
+            printf("Forking failed,Child process can't be created \n");
 
-    }else if (fork_return == 0 )
-    {
+        }else if (fork_return == 0 )
+            {
         //child process excution
 
-        execvp(parameters[0], parameters) ;  // pass parameters list to execvp which change the enviroment to another process "the desired command "
-        // just  for formating style  ERROR will be printed if the execvp failed else it won't return to the code again
-        if(strcmp(parameters[0] ,"exit")!
-        =0)
-        printf("ERROR: Wrong Command\n");
+            execvp(parameters[0], parameters) ;  // pass parameters list to execvp which change the enviroment to another process "the desired command "
+            // just  for formating style  ERROR will be printed if the execvp failed else it won't return to the code again
+            if(strcmp(parameters[0] ,"exit")!=0)
+            printf("ERROR: Wrong Command\n");
 
-        exit(0);
+            exit(0);
 
+        }else
+        {
+            //parent process excution
+            // if & is in the command parent process will wait ;
 
-    }else
-    {
-        //parent process excution
-
-
-// if & is in the command parent process will wait ;
-
-        if(wait_flag ==0)
-        wait(NULL);
-        else{
-        wait_flag= 0 ;
-        printf("%d \n",fork_return);
-        }
+            if(wait_flag ==0)
+                wait(NULL);
+            else{
+                wait_flag= 0 ;
+                printf("%d \n",fork_return);
+            }
 
         //Check for exit call
 
-        if ( strcmp(parameters[0] ,"exit")==0 )
-        {
-            // Free the allocated mem for the input
-           free(input);
-            exit(0);
-
+            if ( strcmp(parameters[0] ,"exit")==0 )
+            {
+                // Free the allocated mem for the input
+                free(input);
+                exit(0);
         }
 
     }}
@@ -87,7 +83,7 @@ printf("Forking failed,Child process can't be created \n");
 
 // Free the allocated mem for the input
 free(input);
-    return 0;
+return 0;
 }
 
 
@@ -101,11 +97,9 @@ printf("\033[0m"); // Reset text color
 }
 
 void parsing_command(char*input , char **parameters){
-
+char * first_cmd  = input;
 
  while (*input != '\0') {       /* if not the end of input ....... */
-
-
 
           while (*input == ' ' || *input == '\t' || *input == '\n' ||*input == '&'){
                if(*input == '&'){
@@ -116,17 +110,45 @@ void parsing_command(char*input , char **parameters){
                *input++ = '\0';     /* replace white spaces with 0    */
 
           }
-          *parameters++ = input;          /* save the argument position     */
+          *parameters++ = input;     /* save the argument position     */
+
+
           while (*input != '\0' && *input != ' ' &&
                  *input != '\t' && *input != '\n')
                input++;             /* skip the argument until ...    */
      }
+     if(strcmp(first_cmd,"ls")==0){
+            *(parameters-1) = color_string;
+            *parameters = NULL ;
+            }
+    else
      *(parameters-1) = NULL;                 /* mark the end of argument list  */
+}
 
+
+unsigned char read_error_check(char *input){
+int i = 0 ;
+/** this fuction scan the input to make sure it's not white space **/
+while(*(input + i) != '\0'){
+    if(*(input + i)!=' ' && *(input + i) != '\t' && *(input + i) !='\n' )
+     return 0 ; // return 0 ===> NO ERROR
+    i++;
+}
+return 1 ; //return 1 ====>ERROR
 
 }
 
 
+void read_command(char *input){
+/** this fucntion ask for the input and keep asking if ERROR exists **/
+type_prompt() ;
+fgets(input, 4098,stdin); //Reads the input MAX size is 4098
 
+while(read_error_check(input)){
+    type_prompt();
+    fgets(input, 4098,stdin);
 
+}
+
+}
 
