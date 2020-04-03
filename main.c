65b364bda125497453a47d6165a6c45b51acde81
OmarMGhanem <ghanem374@gmail.com>
2020-04-03 20:12:29 +0200

    add comments and added some text formating  and now shell will print error in case of Wrong command

--- main.c
+++ main.c
@@ -1,27 +1,37 @@
+/**
+###############################################
+##################ALO SHELL####################
+################# MADE BY . ###################
+######### OMAR MOHAMED GHANEM 166 #############
+######### AMR KHALID RAMADAN  177 #############
+############# LOAY EZZAT 199  #################
+**/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
-void type_prompt(void) ;
-void parsing_command(char *input, char **parameters) ;
-unsigned char  wait_flag  = 0 ;
+
+
+void type_prompt(void) ; // Fuction which called to promet the fixed header "ALO SHELL $" also used for color formating
+void parsing_command(char *input, char **parameters) ; // This Function handle the input and point to every argument start
+unsigned char  wait_flag  = 0 ; // simple flag to chech whether the parent MUST wait or not
 int main()
 {
-    pid_t fork_return;
+    pid_t fork_return; // shall hold process id
     char *input ;
     char  *parameters[64];
 
 
-input = malloc(4098);
+input = malloc(4098);   // Dynamic allocation for the input to avoid  memory Runtime errors
 
 
 
-
+// The shell core
     while(1){
             type_prompt() ;
-            fgets(input, 4098,stdin);
+            fgets(input, 4098,stdin); //Reads the input MAX size is 4098
             parsing_command(input ,parameters);
 
 
@@ -29,7 +39,7 @@ input = malloc(4098);
 
     //forking
 
-    fork_return = fork() ;
+    fork_return = fork() ; // save the fork order for furtur checks
 
     if (fork_return == -1)
     {
@@ -39,7 +49,11 @@ printf("Forking failed,Child process can't be created \n");
     {
         //child process excution
 
-        execvp(parameters[0], parameters) ;
+        execvp(parameters[0], parameters) ;  // pass parameters list to execvp which change the enviroment to another process "the desired command "
+        // just  for formating style  ERROR will be printed if the execvp failed else it won't return to the code again
+        if(strcmp(parameters[0] ,"exit")!
+        =0)
+        printf("ERROR: Wrong Command\n");
 
         exit(0);
 
@@ -48,14 +62,22 @@ printf("Forking failed,Child process can't be created \n");
     {
         //parent process excution
 
+
+// if & is in the command parent process will wait ;
+
         if(wait_flag ==0)
         wait(NULL);
         else{
         wait_flag= 0 ;
         printf("%d \n",fork_return);
         }
+
+        //Check for exit call
+
         if ( strcmp(parameters[0] ,"exit")==0 )
-        {   free(input);
+        {
+            // Free the allocated mem for the input
+           free(input);
             exit(0);
 
         }
@@ -63,7 +85,7 @@ printf("Forking failed,Child process can't be created \n");
     }}
 
 
-
+// Free the allocated mem for the input
 free(input);
     return 0;
 }
@@ -72,7 +94,9 @@ free(input);
 
 
 void type_prompt(void){
+printf("\033[0;32m"); // chenge text color to green
 printf("ALOShell $ ");
+printf("\033[0m"); // Reset text color
 
 }
 
@@ -85,7 +109,8 @@ void parsing_command(char*input , char **parameters){
 
           while (*input == ' ' || *input == '\t' || *input == '\n' ||*input == '&'){
                if(*input == '&'){
-                 wait_flag = 1 ;
+
+                 wait_flag = 1 ; /*Check for the & and  replace it and set the flagg */
                }
 
                *input++ = '\0';     /* replace white spaces with 0    */
